073b80aad2dbfda23b3e514b2059f96763073fb7
Serjio2 <130094175+Serjio2@users.noreply.github.com>
2023-10-05 22:10:15 +0000

    Deploying to gh-pages from @ Serjio2/goit-react-hw-08-phonebook@6c131f33f09fef8b599c25d52a8f5a107e9c39f1 🚀

---
+++ static/js/415.a0f926e6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[415],{415:function(e,t,o){o.r(t);var r=o(184);t.default=function(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"Welcome to Phone Book."})})}}}]);
+//# sourceMappingURL=415.a0f926e6.chunk.js.map
